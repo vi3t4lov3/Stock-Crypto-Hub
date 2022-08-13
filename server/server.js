7951c0f58ac5fa7cddf2a1de9309ef7e4a0ff062
Tu Nguyen <tuinfor@ymail.com>
2022-08-13 13:13:06 -0400

    heroku

--- server/server.js
+++ server/server.js
@@ -17,6 +17,15 @@ app.use(express.static('public'));
 
 app.use('/uploads', express.static('uploads'));
 
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// app.use(routes);
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 //middleware
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
